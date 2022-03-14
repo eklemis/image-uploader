785d9e60129a2b7b85a673570a7b0a3952f7db8f
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2022-03-14 13:23:58 +0800

    create first screen component

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Screen from "../components/screen";
+import { useState } from "react";
 
 export default function Home() {
+	// 0: start screen, 1: uploading screen, 2: result screen
+	const [activeScreen, setActiveScreen] = useState(0);
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -13,11 +16,25 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<Screen>
-					<h1 className={styles.title}>
-						Welcome to <a href="https://nextjs.org">Next.js!</a>
-					</h1>
-				</Screen>
+				{activeScreen === 0 && (
+					<Screen>
+						<h1>Upload your image</h1>
+						<p>File should be Jpeg, Png,...</p>
+						<div>
+							<figure className={styles.figure}>
+								<Image
+									src="/add_to_photos_black_24dp.svg"
+									alt="Trulli"
+									width={114}
+									height={89}
+								/>
+								<figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+							</figure>
+						</div>
+						<p>Or</p>
+						<button>Choose file</button>
+					</Screen>
+				)}
 			</main>
 		</div>
 	);
